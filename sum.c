8bdfabba9a034cc49b61b838c5b0ff2688d10f5c
Himadry Mondol <164491447+H-RUSH@users.noreply.github.com>
2024-03-23 19:12:13 +0600

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter a number =");
+    scanf ("%d%d",&a,&b);
+    printf("you enter a number that is a= %d.\n",a);
+    printf(" u put the vaue b=%d\n.",b);
+    sum=a+b;
+    printf(" the sum is =%d.",sum);
+    return 0;
+
+}
