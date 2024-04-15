90b46626e10882c28839950959f8a4c33ac95587
Himadry Mondol <164491447+H-RUSH@users.noreply.github.com>
2024-04-16 01:13:12 +0600

    Add files via upload

---
+++ if statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf("enter a number");
+ scanf("%d", &a);
+ if(a>=18){
+    printf("you are adult");
+ }
+ else{
+    printf("you are not adult");
+ }
+return 0;
+}
