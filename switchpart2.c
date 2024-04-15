36fa0434e7e612f4d8eba0899f159c5a120f3ee6
Himadry Mondol <164491447+H-RUSH@users.noreply.github.com>
2024-04-16 01:57:30 +0600

    Add files via upload

---
+++ switchpart2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("please enter any number=\n");
+    scanf("%d",&a);
+    switch(a)
+    {
+    case 1:
+        printf("i got you");
+        break;
+        case 2:
+        printf("sorry darling");
+        break;
+        case 3:
+        printf("i got you");
+        break;
+        case 4:
+        printf("otilia dimante");
+        break;
+        case 5:
+        printf("dua lipa night");
+        break;
+        case 6:
+        printf("honey shing birthday bash");
+        break;
+        case 7:
+        printf("kasasthar");
+        break;
+        default:
+            printf("go fuck ur self");
+    }
+    return 0;
+}
