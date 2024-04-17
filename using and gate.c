cebc9ff90a0103a98d77795652b89daddaec1c77
Himadry Mondol <164491447+H-RUSH@users.noreply.github.com>
2024-04-17 18:39:06 +0600

    Add files via upload

---
+++ using and gate.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the value of a=\n");
+    scanf("%d",&a);
+    if(a>0 && a<100){
+        printf("i love you ");
+    }
+    else
+    {
+        printf("i hate you");
+    }
+    return 0;
+}
